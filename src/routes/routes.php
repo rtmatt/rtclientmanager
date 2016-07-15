821f51360a4e5a52135faec110cbc626594d3c0b
Matt Emrick <matt@redtrainwebservices.com>
2016-07-15 15:22:16 -0600

    RMSD-81 removed unnecessary resource routes

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -11,14 +11,14 @@ Route::group([ 'prefix' => 'api/client-manager' ], function () {
     Route::resource('clients.service-plan','\RTMatt\MonthlyService\Controllers\Resource\ClientServicePlanController',['only'=>['index']]);
     Route::resource('clients.service-plan.benefits', '\RTMatt\MonthlyService\Controllers\Resource\ClientServicePlanBenefitController',['only'=>['update','store','destroy']]);
 
-    Route::resource('service-plan','\RTMatt\MonthlyService\Controllers\Resource\ServicePlanController');
+    Route::resource('service-plan','\RTMatt\MonthlyService\Controllers\Resource\ServicePlanController',['only'=>['update']]);
     Route::controller('service-plan/{service_plan}','\RTMatt\MonthlyService\Controllers\Resource\ServicePlanController');
 
-    Route::resource('service-month','\RTMatt\MonthlyService\Controllers\Resource\ServiceMonthController');
+    Route::resource('service-month','\RTMatt\MonthlyService\Controllers\Resource\ServiceMonthController',['only'=>['update']]);
 
-    Route::resource('service-benefit','\RTMatt\MonthlyService\Controllers\Resource\BenefitController');
+    Route::resource('service-benefit','\RTMatt\MonthlyService\Controllers\Resource\BenefitController',['only'=>['update','store','destroy']]);
 
-    Route::resource('priority-alert','\RTMatt\MonthlyService\Controllers\Resource\PriorityAlertController');
+    Route::resource('priority-alert','\RTMatt\MonthlyService\Controllers\Resource\PriorityAlertController',['only'=>['index','store']]);
 
     Route::controller('/', '\RTMatt\MonthlyService\Controllers\API\ClientManagerAPIController');
 });
