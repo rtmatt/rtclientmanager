de66533d41380053cc0ebd7333f79f1dcefc1dd3
Matt Emrick <matt@redtrainwebservices.com>
2016-07-15 22:51:10 -0600

    layout file and section selection

--- config/rtclientmanager.php
+++ config/rtclientmanager.php
@@ -3,5 +3,7 @@
 return [
     'guard' => \RTMatt\MonthlyService\Middleware\RTAPIGuard::class,
     'client_manager_url' => 'client-manager',
+    'layout_file'=>'layouts.admin',
+    'layout_section'=>'content'
     //@todo: this is where config for registering the dashboard in the servide provider would live
 ];
